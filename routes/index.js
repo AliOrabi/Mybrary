d90771f1e0d4b9adf843cc92a9a94039965c86f2
AliOrabi <aliorabi0@gmail.com>
2021-07-18 11:02:31 +0200

    Books/Create/Add/New

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-// All Authors Route
-router.get('/', (req, res) =>{
-    res.render('authors/index')
-})
-
-// New Author Route
-router.get('/new', (req, res) => {
-    res.render('authors/new')
-})
-
-router.post('/', (req, res) => {
-    res.send('Create')
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
 })
 
 module.exports = router
